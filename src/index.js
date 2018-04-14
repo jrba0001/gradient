da5d4ea5c33f1fc6c5d93b0c7b605abd9f40b786
Jose Bonilla <joser.bonilla.sspa@juntadeandalucia.es>
2018-04-14 09:53:37 +0200

    añadido nodemon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import Express from "express";
 const app = new Express();
 
 app.get("/", (req, res, err) => {
-  return res.send("hola mundo cruel");
+  return res.send(`<html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body>
+      <div id="app">
+      <h1>Hola</h1>
+      </div>
+
+    </body>
+    </html>`);
 });
 
 app.listen(3000, () => {
