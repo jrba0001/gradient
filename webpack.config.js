1c9593fe5bd4b807d7f62d07f6e51d0ade9c8934
Jose Bonilla <joser.bonilla.sspa@juntadeandalucia.es>
2018-04-14 11:49:41 +0200

    problemas con webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/client/app.js",
+  resolveLoader: {
+    modules: ["node_modules"]
+  },
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"]
+  },
   output: {
     path: __dirname + "/public/js",
     filename: "app.min.js"
